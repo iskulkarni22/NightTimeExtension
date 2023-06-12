1802d6db382ccaa5a97cc217d77f6724ce04740d
iskulkarni22 <99928280+iskulkarni22@users.noreply.github.com>
2023-06-12 15:49:05 -0400

    Add files via upload

--- appOff.js
+++ appOff.js
@@ -1,7 +1,7 @@
 (function () {
     let html = document.querySelector("html")
     html.style.filter = "invert(0) hue-rotate(0deg)";
-    let media = document.querySelectorAll("img, picture, video, content");
+    let media = document.querySelectorAll("img, picture, video, content, label, svg");
 
     media.forEach((mediaItem) => {
         mediaItem.style.filter = "invert(0) hue-rotate(0deg)";
--- appOn.js
+++ appOn.js
@@ -2,7 +2,7 @@
     let html = document.querySelector("html");
     html.style.filter = "invert(1) hue-rotate(180deg)";
 
-    let media = document.querySelectorAll("img, picture, video, content");
+    let media = document.querySelectorAll("img, picture, video, content, label, svg");
 
     media.forEach((mediaItem) => {
         mediaItem.style.filter = "invert(1) hue-rotate(180deg)";
